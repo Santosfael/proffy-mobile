684878ab760ad495fe7bf88f373a1ec6a4c75e55
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-09-28 17:15:55 -0300

    feat: implementação da tela de recuperar senha

--- app/src/main/java/com/rafael/proffy/LoginActivity.kt
+++ app/src/main/java/com/rafael/proffy/LoginActivity.kt
@@ -1,36 +1,30 @@
 package com.rafael.proffy
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.rafael.proffy.databinding.ActivitySplashScreenBinding
+import com.rafael.proffy.databinding.ActivityLoginBinding
 
-class SplashScreenActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
         enableEdgeToEdge()
-        //setContentView(R.layout.activity_splash_screen)
+        window.statusBarColor = this.getColor(R.color.purple)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler(mainLooper).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 1000)
     }
 }
